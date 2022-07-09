2533b0b8d294048ab31105fce5e5e08a5e2c1bc0
eehaapb <eemil.haaparanta@gmail.com>
2022-07-09 18:01:51 +0300

    removed unnecessary bullshit

--- osa_1/luvut.c
+++ osa_1/luvut.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-const unsigned char PRINT_MAX = 100;
 
 int main(int argc, char** argv) {
-    long int loop_max = argv[1];
-
-    int i = 0;
-    for(i; i <= PRINT_MAX; i++) {
+    int i = 1;
+    
+    for(i; i <= 100; i++) {
         if(i % 2 != 0 && i % 3 != 0 && i % 5 != 0) {
             printf("%d ", i);
         }
