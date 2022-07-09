f119f2e7796c5c81cb5bec35fa9a3c64bede13dd
eehaapb <eemil.haaparanta@gmail.com>
2022-07-09 17:57:48 +0300

    print fixing for submissioning

--- osa_1/luvut_2.c
+++ osa_1/luvut_2.c
@@ -4,6 +4,12 @@
 int main(int argc, char** argv) {
     unsigned int a = atoi(argv[1]);
     unsigned int b = atoi(argv[2]);
+    int frst_prnt = 0;
+    int flag = 0;
+
+    if(a == b) {
+        return 1;
+    }
 
     for(a; a <= b; a++){
         int count = 0;
@@ -14,9 +20,13 @@ int main(int argc, char** argv) {
             }
         }
         if(count == argc - 3) {
-            printf("%d ", a);
+            frst_prnt != 0 ? printf(" %d", a) : printf("%d", a);
+            frst_prnt = 1;
+            flag = 1;
         }
     }
+
+    flag != 0 ? printf("\n") : 0;
 
     return 0;
 }
